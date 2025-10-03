dc65ac7ca900f3e39aef30fee2055b2fad6aaa2d
nguynkhn <nguynkhan479@gmail.com>
2025-10-03 19:49:05 +0700

    update: core functions

--- minigox.c
+++ minigox.c
@@ -1,24 +1,61 @@
-#ifndef _WIN32
-#error "This program is Windows-only"
-#else
-
 #include "minigox.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-#include <windows.h>
+#define ARRAY_LEN(_a)   (sizeof(_a) / sizeof(_a[0]))
 
-static struct CharInfo curr = {0};
-static struct Method method = {
-    .keystroke_num = ARRAYSIZE(TELEX),
-    .keystrokes = TELEX,
-};
+#include <Windows.h>
 
-static LONG mouse_x, mouse_y;
+static struct CharInfo curr;
+static struct Method method;
+static struct {
+    long x, y;
+} mouse;
 static struct {
     int ctrl, alt, shift, caps, num, scroll;
-} modifiers = {0};
+} modifiers;
+
+static bool minigox_setup(void);
+static bool minigox_run(void);
+static void minigox_destroy(void);
+
+int main(int argc, char *argv[]) {
+    method.keystroke_num = ARRAY_LEN(TELEX);
+    method.keystrokes = TELEX;
+
+    if (argc > 1) {
+        char *name = argv[1];
+
+        if (strcmp(name, "vni") == 0) {
+            method.keystroke_num = ARRAY_LEN(VNI);
+            method.keystrokes = VNI;
+        } else if (strcmp(name, "telex") != 0) {
+            fprintf(stderr, "Unknown method name\n");
+            return 1;
+        }
+    }
+
+    if (!minigox_setup()) {
+        fprintf(stderr, "Failed to setup minigox\n");
+        return 1;
+    }
+
+    printf("minigox is ready\n");
+    if (!minigox_run()) {
+        fprintf(stderr, "Unexpected error while running\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+#if defined(_WIN32)
+
+#include <Windows.h>
+
+static HHOOK keyboard_hook, mouse_hook;
 static INPUT input[6] = {
     {
         .type = INPUT_KEYBOARD,
@@ -30,6 +67,7 @@ static INPUT input[6] = {
     },
 };
 
+
 static int process_key(char key) {
     enum ApplyResult result = minigox_apply_method(&method, &curr, key);
     if (result == APPLY_UNCHANGED)
@@ -37,7 +75,7 @@ static int process_key(char key) {
 
     wchar_t ch = curr.base;
     char unicode[4] = {0};
-    if (minigox_compose_char(curr, unicode) == 0) {
+    if (minigox_write_char(curr, unicode) == 0) {
         MultiByteToWideChar(CP_UTF8, 0, unicode, -1, &ch, 1);
     } else {
         goto skip;
@@ -158,21 +196,21 @@ skip:
     return CallNextHookEx(NULL, ncode, wparam, lparam);
 }
 
-LRESULT CALLBACK mouse_proc(int ncode, WPARAM wparam, LPARAM lparam) {
+static LRESULT CALLBACK mouse_proc(int ncode, WPARAM wparam, LPARAM lparam) {
     if (ncode == HC_ACTION) {
-        PMSLLHOOKSTRUCT mouse = (PMSLLHOOKSTRUCT)lparam;
+        PMSLLHOOKSTRUCT _mouse = (PMSLLHOOKSTRUCT)lparam;
 
         switch (wparam) {
         case WM_LBUTTONDOWN:
         case WM_RBUTTONDOWN:
-            if (mouse->pt.x != mouse_x && mouse->pt.y != mouse_y)
+            if (_mouse->pt.x != mouse.x && _mouse->pt.y != mouse.y)
                 curr = minigox_unpack_char(0);
 
             break;
         case WM_LBUTTONUP:
         case WM_RBUTTONUP:
-            mouse_x = mouse->pt.x;
-            mouse_y = mouse->pt.y;
+            mouse.x = _mouse->pt.x;
+            mouse.y = _mouse->pt.y;
             break;
         }
     }
@@ -180,37 +218,29 @@ LRESULT CALLBACK mouse_proc(int ncode, WPARAM wparam, LPARAM lparam) {
     return CallNextHookEx(NULL, ncode, wparam, lparam);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        char *name = argv[1];
+static bool minigox_setup(void) {
+    keyboard_hook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_proc, NULL, 0);
+    mouse_hook = SetWindowsHookEx(WH_MOUSE_LL, mouse_proc, NULL, 0);
 
-        if (strcmp(name, "vni") == 0) {
-            method.keystroke_num = ARRAYSIZE(VNI);
-            method.keystrokes = VNI;
-        } else if (strcmp(name, "telex") != 0) {
-            fprintf(stderr, "Unknown method name\n");
-            return 1;
-        }
-    }
+    return keyboard_hook != NULL && mouse_hook != NULL;
+}
 
-    HHOOK kbd_hook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_proc, NULL, 0);
-    HHOOK mouse_hook = SetWindowsHookEx(WH_MOUSE_LL, mouse_proc, NULL, 0);
-    if (kbd_hook == NULL || mouse_hook == NULL) {
-        fprintf(stderr, "Failed to install hook\n");
-        return 1;
-    }
-
-    printf("minigox is ready\n");
-
+static bool minigox_run(void) {
     MSG msg;
+
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    UnhookWindowsHookEx(kbd_hook);
-    UnhookWindowsHookEx(mouse_hook);
-    return 0;
+    return true;
 }
 
-#endif // _WIN32
+static void minigox_destroy(void) {
+    UnhookWindowsHookEx(keyboard_hook);
+    UnhookWindowsHookEx(mouse_hook);
+}
+
+#else // _WIN32
+#error "No implementation found"
+#endif
